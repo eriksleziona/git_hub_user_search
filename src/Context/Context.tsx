a132a05fdb1e402eff0237c3ee1f0ee2e1c893b9
Erik Sleziona <erik.sleziona@gmail.com>
2024-04-05 15:00:15 +0200

    Adding some TODO

--- src/Context/Context.tsx
+++ src/Context/Context.tsx
@@ -12,11 +12,16 @@ const AppContext = createContext<AppContextType | null>(null);
 
 /*const getUser = async (userName: string = "octocat") => {
     #TODO : Implement the API call to the Github api to get the user. 
+    try : {
+
+    }except (error): 
+        set Error msg
     Default value has to be set to octocat and triggerer by clicking search bnt.
 };*/
 
 export const AppProvider: FC<PropsWithChildren> = ({ children }) => {
   const [isDark, setIsDark] = useState(false);
+  // #TODO : Setting the Error message if the API call Fail.
   const [userName, setUserName] = useState("");
 
   return (
